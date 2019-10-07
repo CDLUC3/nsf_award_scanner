2940a51cfa17bb8e5297b6e23d1bcab13f607b0e
briri <brian.riley@ucop.edu>
2019-10-07 09:58:48 -0700

    added some error handliong

--- application.rb
+++ application.rb
@@ -24,45 +24,55 @@ get '/scan' do
   scanned = []
 
   stream do |out|
-    plans = dmphub.data_management_plans
-    out << 'No DOIs found<br>' if plans.empty?
+    begin
+      plans = dmphub.data_management_plans
+      out << 'No DOIs found<br>' if plans.empty?
 
-    plans.each do |plan|
-      doi = plan['uri'].gsub('http://localhost:3003/api/v1/data_management_plans/', '')
+      plans.each do |plan|
+        doi = plan['uri'].gsub('http://localhost:3003/api/v1/data_management_plans/', '')
 
-#next unless ['10.80030/9ddh-tf78', '10.80030/0cd0-ce69', '10.80030/yxcw-kh07'].include?(doi)
-#next unless ['10.80030/yxcw-kh07'].include?(doi)
+  #next unless ['10.80030/9ddh-tf78', '10.80030/0cd0-ce69', '10.80030/yxcw-kh07'].include?(doi)
+  #next unless ['10.80030/yxcw-kh07'].include?(doi)
 
-      next if processed.include?(doi)
+        next if processed.include?(doi)
 
-      out << "Scanning Awards API for DMP: `#{plan['title']}` (#{doi})<br>"
-      out << "&nbsp;&nbsp;&nbsp;&nbsp;with author(s): #{plan['authors'].gsub('|', ' from ')}<br>"
+        out << "Scanning Awards API for DMP: `#{plan['title']}` (#{doi})<br>"
+        out << "&nbsp;&nbsp;&nbsp;&nbsp;with author(s): #{plan['authors'].gsub('|', ' from ')}<br>"
 
-      award = nsf.find_award_by_title(plan: plan) || {}
-      scanned << doi
+        begin
+          award = nsf.find_award_by_title(plan: plan) || {}
+          scanned << doi
 
-      out << '&nbsp;&nbsp;no matches found<br>' if award.empty?
-      if award.any?
-        out << "&nbsp;&nbsp;<strong>Found award</strong>: <a href=\"#{award[:award_id]}\">#{award[:award_id]}</a><br>"
-        out << "&nbsp;&nbsp;&nbsp;&nbsp;<strong>Title</strong>: #{award[:title]}<br>"
-        award[:principal_investigators].each do |pi|
-          out << "&nbsp;&nbsp;&nbsp;&nbsp;<strong>Investigator</strong>: #{pi[:name]} from #{pi[:organization]}<br>"
-        end
+          out << '&nbsp;&nbsp;no matches found<br>' if award.empty?
+          if award.any?
+            out << "&nbsp;&nbsp;<strong>Found award</strong>: <a href=\"#{award[:award_id]}\">#{award[:award_id]}</a><br>"
+            out << "&nbsp;&nbsp;&nbsp;&nbsp;<strong>Title</strong>: #{award[:title]}<br>"
+            award[:principal_investigators].each do |pi|
+              out << "&nbsp;&nbsp;&nbsp;&nbsp;<strong>Investigator</strong>: #{pi[:name]} from #{pi[:organization]}<br>"
+            end
 
-        out << "<br>&nbsp;&nbsp;Sending award information back to the DMPHub<br>"
-        if dmphub.register_award(dmp: plan, award: award)
-          out << "&nbsp;&nbsp;&nbsp;&nbsp;Sucess"
-        else
-          out << "&nbsp;&nbsp;&nbsp;&nbsp;Something went wrong"
+            out << "<br>&nbsp;&nbsp;Sending award information back to the DMPHub<br>"
+            if dmphub.register_award(dmp: plan, award: award)
+              out << "&nbsp;&nbsp;&nbsp;&nbsp;Sucess"
+            else
+              out << "&nbsp;&nbsp;&nbsp;&nbsp;Something went wrong"
+            end
+          end
+          out << "<hr>"
+
+        rescue StandardError => se
+          out << "&nbsp;&nbsp;<strong>ERROR</strong>: #{se.message}"
+          out << "<hr>"
+          next
         end
       end
-      out << "<hr>"
-    end
 
-p "PROC"
-p scanned
-p "TOTAL"
-p processed + scanned
+  p "PROC"
+  p scanned
+  p "TOTAL"
+  p processed + scanned
+
+
 
     # Always write out the processed file even if code is interrupted!
     file = File.open("#{Dir.pwd}/processed.yml", 'w')
